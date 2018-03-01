5fd7f1e38c380c8a71f8db61a465f5b10749562d
Blefler <blefler@mail.csuchico.edu>
2018-02-28 17:32:06 -0800

    I wonder if anyone reads this. Just in case though, the program has been completed.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,21 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int playtime;
+  int won=0;
+
+  cout<<"How many times would you like to play?"<<endl;
+  cin>>playtime;
+  for (int i=0; i<playtime; i++)
+  {
+  game.newGame();
+  game.guessDoorC();
+  game.guessDoorC();
+    if (game.isWinner() == true)
+    {
+    won++;
+    }
+  }
+  cout<<won<<endl;
   return 0;
 }
